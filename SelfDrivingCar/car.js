41a255c56f7e2e8915a7e168999b2d60b7d7943c
Xander Palmer <xanderpalmer7@gmail.com>
2022-07-19 21:11:14 -0400

    Finished Simulating Traffic

--- SelfDrivingCar/car.js
+++ SelfDrivingCar/car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, controlType, maxSpeed=3) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -7,31 +7,43 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
         this.damaged = false;
+        
+        if(controlType != "DUMMY") {
+            this.sensor = new Sensor(this);
+        }
 
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        this.controls = new Controls(controlType);
     }
 
-    update(roadBoarders) {
+    update(roadBoarders, traffic) {
             if(!this.damaged){
             this.#move()
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBoarders);
+            this.damaged = this.#assessDamage(roadBoarders, traffic);
         }
-
-        this.sensor.update(roadBoarders);
+        if(this.sensor) {
+            this.sensor.update(roadBoarders, traffic);
+        }
     }
 
-    #assessDamage(roadBoarders) {
+    #assessDamage(roadBoarders, traffic) {
         for(let i = 0; i < roadBoarders.length; i++) {
             if(polysIntersect(this.polygon, roadBoarders[i])) {
                 return true;
             }
         }
+
+        for(let i = 0; i < traffic.length; i++) {
+            console.log(traffic[i].length)
+            if(polysIntersect(this.polygon, traffic[i].polygon)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -98,11 +110,11 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx) {
+    draw(ctx, color) {
         if(this.damaged) {
             ctx.fillStyle = "gray";
         } else {
-            ctx.fillStyle = "black";
+            ctx.fillStyle = color;
         }
 
         ctx.beginPath();
@@ -112,7 +124,8 @@ class Car {
         }
 
         ctx.fill();
-
-        this.sensor.draw(ctx);
+        if(this.sensor) {
+            this.sensor.draw(ctx);
+        }
     }
 }
